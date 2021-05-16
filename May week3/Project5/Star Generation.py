6b249bb96763683aec99cfdac6e8deb596b05f01
Ivankipsit <ivankipsit@gmail.com>
2021-05-16 16:24:59 +0530

    5 done

--- May week3/Project5/Star Generation.py
+++ May week3/Project5/Star Generation.py
@@ -1,3 +1,9 @@
+"""
+Star Generation: Create a python function which takes a number N and generates following star
+pattern accordingly. N ranges from 1 to any positive number. Make sure if N is not passed as
+argument while calling function, it should take 3 as it’s default value.
+"""
+
 def star(N = 3):
     for _ in range(1,N+1):
         if _%2 == 0:
