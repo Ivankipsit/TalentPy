3032ccb4cb32d9c45aefb3420fc9ee460658549c
Ivankipsit <ivankipsit@gmail.com>
2021-05-16 22:15:27 +0530

    1st pending

--- May week3/Project1/1 or 89 application.py
+++ May week3/Project1/1 or 89 application.py
@@ -3,7 +3,7 @@ Mr.Talentpy would like to create a func,on one_or_eight which takes an integer i
 performs following opera,on.
 1. Square the number if it is single digit. (Eg: 3, then 3 * 3 = 9)
 2. If it is not single digit, square each digits and add. (Eg: 12, then (1*1) + (2*2) = 1+4 = 5
-You have to repeat step (1) and (2) un,l you reach 1 or 89. Note that, always your result will reach
+You have to repeat step (1) and (2) until you reach 1 or 89. Note that, always your result will reach
 1 or 89 for sure. Input must be a posi,ve number.
 If the opera,on reaches at the end, 89 return True, if opera,on reaches 1 at the end return False.
 """
