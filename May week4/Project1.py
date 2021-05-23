12c9d4f36140b366b5c3ff5ca08b227ab4ec965a
Ivankipsit <ivankipsit@gmail.com>
2021-05-23 11:53:23 +0530

    Proj1 Updated

--- May week4/Project1.py
+++ May week4/Project1.py
@@ -3,12 +3,7 @@ Create a lambda function which takes two inputs X and Y and performs X power Y
 operation and returns the output. For Example: If X is 2 and Y is 3, then 2 power 3 = 2
 * 2 * 2 = 8.
 """
-x = input().split()
-y = input().split()
-X = [int(a) for a in x]
-Y = [int(a) for a in y]
-print(X)
-print(Y)
-new_list = []
-lam_obj = list(map(lambda X,Y: X**Y, new_list))
-print(new_list)
+X = int(input())
+Y = int(input())
+lam_obj = lambda X, Y: X ** Y
+print(f"{X} power {Y} = {lam_obj(X,Y)}")
