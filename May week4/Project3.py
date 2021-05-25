0e2c2c20e82b745fb28b4893f3e741898bca4e50
Ivankipsit <ivankipsit@gmail.com>
2021-05-25 20:08:03 +0530

    P3 updated

---
+++ May week4/Project3.py
@@ -0,0 +1,15 @@
+"""
+Write a generator to get even numbers from 1 to infinity.
+"""
+import time
+try:
+    x = 0
+    def gen_func(x):
+        yield x
+    while True:
+        x+=1
+        a = list(gen_func(x))
+        print(a[0])
+        time.sleep(1) 
+except KeyboardInterrupt:
+    print("Loop Exited")
