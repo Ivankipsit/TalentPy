6bc147d99c90bf84e57c8a540ff9f425993883b3
Ivankipsit <68858854+Ivankipsit@users.noreply.github.com>
2021-06-07 21:30:11 +0530

    Update MiniProj.py

--- May week5/MiniProject/MiniProj.py
+++ May week5/MiniProject/MiniProj.py
@@ -1,1 +1,7 @@
-#
+class Doctor:
+    def __init__(self, name, specialisations, availability_days, appoinment_seat, class_hospilal):
+        self.name = name
+        self.specialisations = specialisations
+        self.available_days = available_days
+        self.appoinment_seat = appoinment_seat
+        #need to add hospital initiators
